35c75af3f92cad4e2f8c146bb73dd729ef258df1
Danilo Camargo <danilo@pod99.com>
2026-09-15 14:28:17 +0000

    fix: Adicionar SqsParameters obrigatório na configuração do EventBridge target
    - EventBridge require SqsParameters com RoleArn para enviar mensagens para SQS
    - Corrige erro: 'Parameter(s) SqsParameters must be specified for target: 1'

--- scripts/setup-eventbridge-sqs.sh
+++ scripts/setup-eventbridge-sqs.sh
@@ -49,7 +49,10 @@ TARGET_RESPONSE=$(curl -s -X POST http://localhost:4566/ \
     "Targets": [{
       "Id": "1",
       "Arn": "arn:aws:sqs:us-east-1:000000000000:pod99-accounting-queue.fifo",
-      "RoleArn": "arn:aws:iam::000000000000:role/pod99-eventbridge-role"
+      "RoleArn": "arn:aws:iam::000000000000:role/pod99-eventbridge-role",
+      "SqsParameters": {
+        "RoleArn": "arn:aws:iam::000000000000:role/pod99-eventbridge-role"
+      }
     }]
   }')
 
